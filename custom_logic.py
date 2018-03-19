7baa39f5f9c588844ba63306ced64dbbe89c5687
Rozhdestvenskiy Dmitriy <dremsama@gmail.com>
2018-03-19 17:21:36 +0300

    v0.2
    Working primitiv of selecting cases and storing them into DB

--- custom_logic.py
+++ custom_logic.py
@@ -1,14 +1,19 @@
 from simple_salesforce import Salesforce
+import pyodbc
+import configuration
+import logging
 
 
-def find_cases_with_potential_sla(sf_connection: Salesforce)->list:
-    case_check_query = "SELECT id, OwnerId, Status, CaseNumber, Previous_Owner__c, CreatedDate, Subject, AccountId, Flag__c, Manager_of_Case_Owner__c from case WHERE Time_to_Respond__c < 100 and Time_to_Respond__c > 0 and status in ('New', 'Open') and FTR_Case_Owner__c = null"
+def find_cases_with_potential_sla(sf_connection: Salesforce, max_allowed_sla: int = 60)->list:
+    if max_allowed_sla < 0:
+        max_allowed_sla = 60
+    case_check_query = "SELECT id, OwnerId, Status, CaseNumber, Previous_Owner__c, CreatedDate, Subject, AccountId, Flag__c, Manager_of_Case_Owner__c from case WHERE Time_to_Respond__c < "+str(max_allowed_sla)+" and Time_to_Respond__c > 0 and status in ('New', 'Open') and FTR_Case_Owner__c = null"
     found_cases = sf_connection.query(query=case_check_query)
     found_cases_list = []
     for row in found_cases['records']:
         case_info = {
             'CaseNumber': row['CaseNumber'],
-            'id': row['Id'],
+            'Id': row['Id'],
             'OwnerId': row['OwnerId'],
             'Status': row['Status'],
             'CreatedDate': row['CreatedDate'],
@@ -20,4 +25,38 @@ def find_cases_with_potential_sla(sf_connection: Salesforce)->list:
         }
         found_cases_list.append(case_info)
     return found_cases_list
+
+
+class SQLConnector:
+    def __init__(self, sql_config: configuration.SQLConfig):
+        self.connection = pyodbc.connect(
+            'DRIVER=' + sql_config.Driver + ';PORT=1433;SERVER=' + sql_config.Server + ';PORT=1443;DATABASE='
+            + sql_config.Database + ';UID=' + sql_config.Username + ';PWD=' + sql_config.Password)
+        self.cursor = self.connection.cursor()
+        self.logging_inst = logging.getLogger()
+
+    def insert_into_dbo_cases(self, case_dict: dict)->bool:
+        CaseId = case_dict['Id']
+        CaseNumber = case_dict['CaseNumber']
+        OwnerId = case_dict['OwnerId']
+        Status = case_dict['Status']
+        CreatedDate = case_dict['CreatedDate'][:-5]
+        Subject = case_dict['Subject']
+        AccountId = case_dict['AccountId']
+        Flag = str(case_dict['Flag__c'])
+        Previous_Owner = case_dict['Previous_Owner__c']
+        Manager_of_Case_Owner = case_dict['Manager_of_Case_Owner__c']
+        case_tuple = (CaseNumber, OwnerId, CaseId, CreatedDate,  Status, Subject, AccountId, Flag, Previous_Owner, Manager_of_Case_Owner)
+        try:
+            self.cursor.execute(
+                "insert into [SLA_bot].[dbo].[Cases] values(NEWID(),?,?,?,?,GETDATE(), 0, Null, Null,?,?,?,?,?,?)", *case_tuple[0:10])
+            self.connection.commit()
+            self.logging_inst.info(
+                'insertion of case ' + case_dict['CaseNumber'] + ' was completed')
+            return True
+        except Exception as error:
+            self.connection.rollback()
+            self.logging_inst.error('insertion of case ' + str(CaseNumber) + ' has failed due to the following error \n'+str(error))
+            return False
+
 
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from simple_salesforce import Salesforce
 import simple_salesforce
-from configuration import SFConfig, SFQueues
+from configuration import SFConfig, SFQueues, SQLConfig
 import logging
 from datetime import datetime
 import custom_logic
 
+##############################################################
+#                        variables                           #
+MaxAllowedSLA = 60
+#                                                            #
+##############################################################
 
 def initialize(sf_config_inst: SFConfig, log_level: str = 'INFO', log_to_file: bool = False):
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -19,6 +24,7 @@ def initialize(sf_config_inst: SFConfig, log_level: str = 'INFO', log_to_file: b
     ch = logging.StreamHandler()
     ch.setLevel(log_level)
     ch.setFormatter(formatter)
+    logger_inst.addHandler(ch)
     try:
         sf_connection = Salesforce(username=sf_config_inst.user, password=sf_config_inst.password, security_token=sf_config_inst.token)
     except simple_salesforce.exceptions.SalesforceAuthenticationFailed as error:
@@ -28,9 +34,17 @@ def initialize(sf_config_inst: SFConfig, log_level: str = 'INFO', log_to_file: b
 
 sf_config_instance = SFConfig()
 sf_queues_instance = SFQueues()
+sql_config_instance = SQLConfig()
+sql_connector_instance = custom_logic.SQLConnector(sql_config_instance)
 MainLogger, SF_connection = initialize(sf_config_inst=sf_config_instance, log_level='INFO', log_to_file=False)
 MainLogger.info('Main process has been initialized')
 MainLogger.info('Searching for new potential SLA violations...')
-found_cases_list = custom_logic.find_cases_with_potential_sla(SF_connection)
-MainLogger.info('Done, found ' + str(len(found_cases_list)) + ' case(s)')
-#print(found_cases_list)
+found_cases_list = custom_logic.find_cases_with_potential_sla(sf_connection=SF_connection, max_allowed_sla=MaxAllowedSLA)
+
+if len(found_cases_list) == 0:
+    MainLogger.info('Done, no threats were found')
+else:
+    MainLogger.info('Done, found ' + str(len(found_cases_list)) + ' case(s)')
+
+for case_dict in found_cases_list:
+    result = sql_connector_instance.insert_into_dbo_cases(case_dict=case_dict)
